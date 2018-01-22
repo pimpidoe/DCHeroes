4a0c04ac0f0053f57816b54eb5f05646b4762067
Pim <79192@rocteraa-student.nl>
2018-01-22 13:43:45 +0100

    Begin

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include 'include/connection.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="DC Heroes">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>DC Heroes - Code Example Layout</title>
+</head>
+<body>
+
+	<header id="header">
+		Header
+	</header>
+
+	<div id="main-container">
+
+		<div id="main-left">
+			Left container
+		</div>
+
+		<div id="main-center">
+			Center container
+		</div>
+
+		<div id="main-right">
+			Right container
+		</div>
+
+	</div>
+
+</body>
+</html>
